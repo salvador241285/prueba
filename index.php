912ada7192175d74bf14c3adfe26a7155e4a817c
salvador <salvador241285@gmail.com>
2019-07-06 08:59:49 -0500

    agregando estructura HTML

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-
-      try {
-        $conn = new PDO("mysql:host=$servername;dbname=empleado", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "conexion Exitosa";
-        }catch(PDOException $e)
-        {
-          echo "Fallo la Conexion: " . $e->getMessage();
-        }
-    ?>
-
-  </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>mi primera pagina</title>
+</head>
+<body>
+  
+</body>
 </html>
